91b174ee3ffe0fb83822087a5595a0f5b8657636
Chad Scira <chadvscira@gmail.com>
2013-01-24 01:11:03 -0800

    Update prefixfree.js
    removed dom stylesheet interaction

--- prefixfree.js
+++ prefixfree.js
@@ -4,141 +4,23 @@
  * MIT license
  */
 
-(function(){
 
-if(!window.addEventListener) {
-	return;
-}
+/**
+ * PrefixFree
+ */
+(function(root){
 
-var self = window.StyleFix = {
-	link: function(link) {
-		try {
-			// Ignore stylesheets with data-noprefix attribute as well as alternate stylesheets
-			if(link.rel !== 'stylesheet' || link.hasAttribute('data-noprefix')) {
-				return;
-			}
-		}
-		catch(e) {
-			return;
-		}
+if(!window.getComputedStyle) return;
 
-		var url = link.href || link.getAttribute('data-href'),
-		    base = url.replace(/[^\/]+$/, ''),
-		    base_scheme = (/^[a-z]{3,10}:/.exec(base) || [''])[0],
-		    base_domain = (/^[a-z]{3,10}:\/\/[^\/]+/.exec(base) || [''])[0],
-		    base_query = /^([^?]*)\??/.exec(url)[1],
-		    parent = link.parentNode,
-		    xhr = new XMLHttpRequest(),
-		    process;
-		
-		xhr.onreadystatechange = function() {
-			if(xhr.readyState === 4) {
-				process();
-			}
-		};
-
-		process = function() {
-				var css = xhr.responseText;
-				
-				if(css && link.parentNode && (!xhr.status || xhr.status < 400 || xhr.status > 600)) {
-					css = self.fix(css, true, link);
-					
-					// Convert relative URLs to absolute, if needed
-					if(base) {
-						css = css.replace(/url\(\s*?((?:"|')?)(.+?)\1\s*?\)/gi, function($0, quote, url) {
-							if(/^([a-z]{3,10}:|#)/i.test(url)) { // Absolute & or hash-relative
-								return $0;
-							}
-							else if(/^\/\//.test(url)) { // Scheme-relative
-								// May contain sequences like /../ and /./ but those DO work
-								return 'url("' + base_scheme + url + '")';
-							}
-							else if(/^\//.test(url)) { // Domain-relative
-								return 'url("' + base_domain + url + '")';
-							}
-							else if(/^\?/.test(url)) { // Query-relative
-								return 'url("' + base_query + url + '")';
-							}
-							else {
-								// Path-relative
-								return 'url("' + base + url + '")';
-							}
-						});
-
-						// behavior URLs shoudn’t be converted (Issue #19)
-						// base should be escaped before added to RegExp (Issue #81)
-						var escaped_base = base.replace(/([\\\^\$*+[\]?{}.=!:(|)])/g,"\\$1");
-						css = css.replace(RegExp('\\b(behavior:\\s*?url\\(\'?"?)' + escaped_base, 'gi'), '$1');
-						}
-					
-					var style = document.createElement('style');
-					style.textContent = css;
-					style.media = link.media;
-					style.disabled = link.disabled;
-					style.setAttribute('data-href', link.getAttribute('href'));
-					
-					parent.insertBefore(style, link);
-					parent.removeChild(link);
-					
-					style.media = link.media; // Duplicate is intentional. See issue #31
-				}
-		};
-
-		try {
-			xhr.open('GET', url);
-			xhr.send(null);
-		} catch (e) {
-			// Fallback to XDomainRequest if available
-			if (typeof XDomainRequest != "undefined") {
-				xhr = new XDomainRequest();
-				xhr.onerror = xhr.onprogress = function() {};
-				xhr.onload = process;
-				xhr.open("GET", url);
-				xhr.send(null);
-			}
-		}
-		
-		link.setAttribute('data-inprogress', '');
-	},
-
-	styleElement: function(style) {
-		if (style.hasAttribute('data-noprefix')) {
-			return;
-		}
-		var disabled = style.disabled;
-		
-		style.textContent = self.fix(style.textContent, true, style);
-		
-		style.disabled = disabled;
-	},
-
-	styleAttribute: function(element) {
-		var css = element.getAttribute('style');
-		
-		css = self.fix(css, false, element);
-		
-		element.setAttribute('style', css);
-	},
-	
-	process: function() {
-		// Linked stylesheets
-		$('link[rel="stylesheet"]:not([data-inprogress])').forEach(StyleFix.link);
-		
-		// Inline stylesheets
-		$('style').forEach(StyleFix.styleElement);
-		
-		// Inline styles
-		$('[style]').forEach(StyleFix.styleAttribute);
-	},
-	
+var StyleFix = {
 	register: function(fixer, index) {
 		(self.fixers = self.fixers || [])
 			.splice(index === undefined? self.fixers.length : index, 0, fixer);
 	},
 	
-	fix: function(css, raw, element) {
+	fix: function(css, raw) {
 		for(var i=0; i<self.fixers.length; i++) {
-			css = self.fixers[i](css, raw, element) || css;
+			css = self.fixers[i](css, raw) || css;
 		}
 		
 		return css;
@@ -153,32 +35,6 @@ var self = window.StyleFix = {
 	}
 };
 
-/**************************************
- * Process styles
- **************************************/
-(function(){
-	setTimeout(function(){
-		$('link[rel="stylesheet"]').forEach(StyleFix.link);
-	}, 10);
-	
-	document.addEventListener('DOMContentLoaded', StyleFix.process, false);
-})();
-
-function $(expr, con) {
-	return [].slice.call((con || document).querySelectorAll(expr));
-}
-
-})();
-
-/**
- * PrefixFree
- */
-(function(root){
-
-if(!window.StyleFix || !window.getComputedStyle) {
-	return;
-}
-
 // Private helper
 function fix(what, before, after, replacement, css) {
 	what = self[what];
@@ -192,7 +48,7 @@ function fix(what, before, after, replacement, css) {
 	return css;
 }
 
-var self = window.PrefixFree = {
+var self = PrefixFree = {
 	prefixCSS: function(css, raw, element) {
 		var prefix = self.prefix;
 		
@@ -483,5 +339,9 @@ root.className += ' ' + self.prefix;
 
 StyleFix.register(self.prefixCSS);
 
+PrefixFree.processCSS = function (css) {
+	return StyleFix.fix(css, true);
+};
 
+return PrefixFree;
 })(document.documentElement);
